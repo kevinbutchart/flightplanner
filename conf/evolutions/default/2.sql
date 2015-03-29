0836d959e1da7907512b9ccdfed7c269fed71674
Kevin Butchart <kevinbutchart@ownmail.net>
2015-03-29 10:42:58 +0100

    fix database

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -2,7 +2,15 @@
 # To stop Slick DDL generation, remove this comment and start using Evolutions
 
 # --- !Ups
-ALTER TABLE "JOURNEY_LEGS" ADD "JOURNEY_ID" INTEGER NOT NULL;
+insert into "PLACES" ("NAME", "LATITUDE", "LONGITUDE") values ('Amersham', 51.6797222222222, -0.600277777777778);
+insert into "PLACES" ("NAME", "LATITUDE", "LONGITUDE") values ('Biggleswade', 52.0839722222222, -0.259944444444444);
+insert into "PLACES" ("NAME", "LATITUDE", "LONGITUDE") values ('Bovingdon', 51.7285, -0.542361111111111);
+insert into "PLACES" ("NAME", "LATITUDE", "LONGITUDE") values ('Cambridge Airport', 52.2045277777778, 0.174138888888889);
+insert into "PLACES" ("NAME", "LATITUDE", "LONGITUDE") values ('Maple Cross', 51.62975, -0.504111111111111);
+insert into "PLACES" ("NAME", "LATITUDE", "LONGITUDE") values ('St Giles', 51.6303888888889, -0.577166666666667);
+insert into "PLACES" ("NAME", "LATITUDE", "LONGITUDE") values ('St Neots', 52.2285277777778, -0.267583333333333);
+insert into "PLACES" ("NAME", "LATITUDE", "LONGITUDE") values ('Tring', 51.7984722222222, -0.660277777777778);
+insert into "PLACES" ("NAME", "LATITUDE", "LONGITUDE") values ('Wing', 51.9043055555556, -0.750833333333333);
+
 
 # --- !Downs
-ALTER TABLE "JOURNEY_LEGS" DROP "JOURNEY_NAME";
